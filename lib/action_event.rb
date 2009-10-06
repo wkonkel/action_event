4a835efd8d255a5fe82d27fe61c21bcced095198
Warren Konkel <wkonkel@gmail.com>
2009-10-06 14:35:47 -0400

    remove debug

--- lib/action_event.rb
+++ lib/action_event.rb
@@ -2,5 +2,3 @@ require 'action_event/base'
 require 'action_event/object_extensions'
 require 'action_event/message'
 ActiveSupport::Dependencies.load_paths << File.join(RAILS_ROOT, 'app/events')
-
-puts "RUNNING INIT"
